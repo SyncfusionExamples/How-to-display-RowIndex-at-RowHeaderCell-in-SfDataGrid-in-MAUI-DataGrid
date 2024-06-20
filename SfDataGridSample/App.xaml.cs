8f4ddb5327605f4a3eb020b58822482f1ba7d016
SethupathyDevarajan <sethupathy.sf3898@gmail.com>
2024-06-20 16:58:58 +0530

    Code updated.

---
+++ SfDataGridSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SfDataGridSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
